dd1f78b15850da0f4d82f7a24436a2b427234ff1
bashar_karoury <basharalkaroury@hotmail.com>
2024-12-25 15:42:07 +0300

    Structe voice transaction components

--- src/app/voice/page.tsx
+++ src/app/voice/page.tsx
@@ -1,49 +1,14 @@
 "use client";
 
-import { useRef, useState } from "react";
+import RecordTransactionButton from "@/Components/RecordTransactionButton";
 
 export default function Page() {
-  let audioChunks: BlobPart[] = [];
-  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
-  const [audioUrl, setAudioUrl] = useState<string | null>(null);
-  const [isRecording, setIsRecording] = useState(false);
-
-  async function startButtonHandler() {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-    mediaRecorderRef.current = new MediaRecorder(stream);
-
-    mediaRecorderRef.current.ondataavailable = (event) => {
-      audioChunks.push(event.data);
-    };
-
-    mediaRecorderRef.current.onstop = () => {
-      const audioBlob = new Blob(audioChunks, { type: "audio/webm" });
-      console.log(audioBlob);
-      audioChunks = [];
-      const newAudioUrl = URL.createObjectURL(audioBlob);
-      console.log(newAudioUrl);
-      setAudioUrl(newAudioUrl);
-    };
-
-    mediaRecorderRef.current.start();
-    setIsRecording(true);
-  }
-
-  async function stopButtonHandler() {
-    mediaRecorderRef.current?.stop();
-    setIsRecording(false);
-  }
-
+  const handleTransactionRecorded = (url: string) => {
+    console.log("Transaction recorded:", url);
+  };
   return (
-    <>
-      <button onClick={startButtonHandler} disabled={isRecording}>
-        startButton
-      </button>
-      ;
-      <button onClick={stopButtonHandler} disabled={!isRecording}>
-        stopButton
-      </button>
-      ;{audioUrl && <audio src={audioUrl} controls />}
-    </>
+    <RecordTransactionButton
+      onTransactionRecorded={handleTransactionRecorded}
+    />
   );
 }
