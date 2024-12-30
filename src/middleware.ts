4ed0847a7ef7e728c79a1fbc4c6e4c8693ea4235
bashar_karoury <basharalkaroury@hotmail.com>
2024-12-30 14:25:26 +0300

    Fix issue of api requests shouldn't be redirected

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,9 @@ import { stackServerApp } from "./stack";
 export async function middleware(request: NextRequest) {
   const user = await stackServerApp.getUser();
   if (!user) {
+    if (request.nextUrl.pathname.startsWith("/api/")) {
+      return new NextResponse("Not authorized", { status: 401 });
+    }
     return NextResponse.redirect(new URL("/handler/sign-in", request.url));
   }
   if (!user?.clientMetadata?.onboarded) {
@@ -15,5 +18,5 @@ export async function middleware(request: NextRequest) {
 export const config = {
   // You can add your own route protection logic here
   // Make sure not to protect the root URL, as it would prevent users from accessing static Next.js files or Stack's /handler path
-  // matcher: ["/protected/:path*", "/api/:path*"],
+  matcher: ["/protected/:path*", "/api/:path*"],
 };
