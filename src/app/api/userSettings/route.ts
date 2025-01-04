58f89353fadd775a0b63dd2c2817b3b4b991b3e9
b7sh <mohammedabdelbasit500@gmail.com>
2025-01-04 21:51:40 +0300

    add a new end point PUT request to the user setttings and update the GET request 'using email instead of the id'

--- src/app/api/userSettings/route.ts
+++ src/app/api/userSettings/route.ts
@@ -4,33 +4,89 @@
 // userSettings route
 
 import dbConnect from '../../../utils/db';
-
 import { NextRequest, NextResponse } from 'next/server';
-import {getUserSettings} from '../../../utils/handler/functions';
+import { getUserIdByEmail, updateUserSettings, getUserSettings } from '../../../utils/handler/functions';
+import { stackServerApp } from '@/stack';
 
+
+// endpoint to get user settings
 export async function GET(req: NextRequest) {
     try {
         await dbConnect();
-        const { searchParams} = new URL(req.url);
-        const _id = searchParams.get('_id');
+
+        // Get the user from the request
+        const user = await stackServerApp.getUser();
+        if (!user) {
+            return new NextResponse("Not authorized", { status: 401 });
+        }
+        // Get the user's email
+        const user_email = user.primaryEmail;
+        console.log('User email:', user_email);
+        if (!user_email) {
+            return new NextResponse("User email not found", { status: 400 });
+        }
+        // Get the user's id using the email
+        const _id: string = await getUserIdByEmail(user_email);
+
+        if (!_id) {
+            return new NextResponse("User not found", { status: 404 });
+        }
         console.log('Request ID:', _id);
 
-        if(!_id){
-            return NextResponse.json(
-                {error: '_id parameter is required'},
-                {status: 400}
-            );
-        }
-
+        // Fetch user settings using '_id'
         const userSettings = await getUserSettings(_id);
         console.log('User Settings:', userSettings);
 
+        // Return the user settings
         return NextResponse.json(userSettings, {status: 200});
     }
     catch(error: any) {
+        // Return an error if the user settings could not be retrieved
         console.log('Error getting user settings:', error);
         return NextResponse.json(
             {error: 'Failed to get user settings', details: error.message},
+            {status: 500}
+        );
+    }
+}
+
+// endpoint to update user settings
+export async function PUT(req: NextRequest) {
+    try {
+        await dbConnect();
+
+        // Get the user from the request
+        const user = await stackServerApp.getUser();
+        if (!user) {
+            return new NextResponse("Not authorized", {status: 401});
+        }
+        // Get the user's email
+        const user_email = user.primaryEmail;
+        console.log('User email:', user_email);
+        if (!user_email) {
+            return new NextResponse("User email not found", {status: 400});
+        }
+        // Get the user's id using the email
+        const _id: string = await getUserIdByEmail(user_email);
+
+        if (!_id) {
+            return new NextResponse("User not found", {status: 404});
+        }
+        console.log('Request ID:', _id);
+
+        // Get the body of the request
+        const body = await req.json();
+        console.log('Request Body:', body);
+
+        const updatedUserSettings = await updateUserSettings(_id, body);
+        console.log('Updated User Settings:', updatedUserSettings);
+
+        return NextResponse.json(updatedUserSettings, {status: 200});
+    }
+    catch(error: any) {
+        console.log('Error updating user settings:', error);
+        return NextResponse.json(
+            {error: 'Failed to update user settings', details: error.message},
             {status: 500}
         );
     }
