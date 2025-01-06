0e400eb9e512783739568e73e3674ff2f4a87114
Minatallasabri <minatallasabri3380@gmail.com>
2025-01-06 09:50:09 +0200

    TryAgainToFixIt at 06-09

--- src/app/SideBarComponent.jsx
+++ src/app/SideBarComponent.jsx
@@ -9,12 +9,10 @@ import {
   ChartPieIcon,
   UserIcon,
 } from "@heroicons/react/24/outline";
-import styles from "./rootLayout.module.css";
-
-import NewNotificationsNumberComponent from "@/Components/NNNComponent";
 import React from 'react';
 import Link from 'next/link';
 import styles from './sidebar.module.css';
+import NewNotificationsNumberComponent from "@/Components/NNNComponent";
 
 function SignOutButton() {
   const user = useUser();
