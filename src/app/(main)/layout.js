f917a34dca47fb43e059a3fb8536999a9b0d90a1
bashar_karoury <basharalkaroury@hotmail.com>
2025-01-08 22:31:51 +0300

    Fix error of multiople html tags

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -2,10 +2,8 @@ import SideBarComponent from '../SideBarComponent';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <SideBarComponent>{children}</SideBarComponent>
-      </body>
-    </html>
+    <>
+      <SideBarComponent>{children}</SideBarComponent>
+    </>
   );
 }
