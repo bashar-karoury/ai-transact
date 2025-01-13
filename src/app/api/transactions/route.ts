3f9312d053891c54eb946d5ee9042f29dff48c40
bashar_karoury <basharalkaroury@hotmail.com>
2025-01-13 20:16:39 +0300

    strip out middleware and db in transactions GET api endpoint

--- src/app/api/transactions/route.ts
+++ src/app/api/transactions/route.ts
@@ -13,9 +13,6 @@ import {
 import { ObjectId } from "mongodb";
 import dbConnect from "../../../utils/db";
 import { stackServerApp } from "@/stack";
-export const runtime = "edge"; // 'nodejs' is the default
-// execute this function on iad1 or hnd1, based on the connecting client location
-export const preferredRegion = ["iad1", "hnd1"];
 
 export async function POST(req: NextRequest) {
   try {
@@ -57,57 +54,57 @@ export async function POST(req: NextRequest) {
 // `GET /transactions?time=today|this_month|this_year`
 export async function GET(req: NextRequest) {
   try {
-    await dbConnect();
+    // await dbConnect();
 
-    const user_email = req.headers.get("x-user-email");
-    if (!user_email) {
-      return new NextResponse("User email not found", { status: 400 });
-    }
-    // Get the user's id using the email
-    const _id: string = await getUserIdByEmail(user_email);
-    console.log("_id=", _id);
-    // const _id = new ObjectId("677cff43a527bafc5d8b7280");
-    // Get the 'time' parameter from the body
-    // const body = await req.json();
-    // const time = body.time;
-    // console.log('Request Body:', body);
+    // const user_email = req.headers.get("x-user-email");
+    // if (!user_email) {
+    //   return new NextResponse("User email not found", { status: 400 });
+    // }
+    // // Get the user's id using the email
+    // const _id: string = await getUserIdByEmail(user_email);
+    // console.log("_id=", _id);
+    // // const _id = new ObjectId("677cff43a527bafc5d8b7280");
+    // // Get the 'time' parameter from the body
+    // // const body = await req.json();
+    // // const time = body.time;
+    // // console.log('Request Body:', body);
 
-    // use the query parameter to get the user's id, and the time parameter to get the time frame
-    const { searchParams } = new URL(req.url);
-    // const _id = searchParams.get('_id');
-    const time = searchParams.get("time");
-    console.log(`time =[${time}]`);
-    // const time = "Past";
-    if (!_id) {
-      // Return an error if the user id is not provided
-      return NextResponse.json(
-        { error: "_id parameter is required" },
-        { status: 400 }
-      );
-    }
+    // // use the query parameter to get the user's id, and the time parameter to get the time frame
+    // const { searchParams } = new URL(req.url);
+    // // const _id = searchParams.get('_id');
+    // const time = searchParams.get("time");
+    // console.log(`time =[${time}]`);
+    // // const time = "Past";
+    // if (!_id) {
+    //   // Return an error if the user id is not provided
+    //   return NextResponse.json(
+    //     { error: "_id parameter is required" },
+    //     { status: 400 }
+    //   );
+    // }
 
-    // console.log("Request ID:", _id);
+    // // console.log("Request ID:", _id);
 
-    let transactions;
+    // let transactions;
 
-    switch (time) {
-      // get transactions for today
-      case "today":
-        transactions = await getTransactionsForToday(_id);
-        break;
-      // get transactions for this month
-      case "this-month":
-        transactions = await getTransactionsForThisMonth(_id);
-        break;
-      // get transactions for this year
-      case "this-week":
-        transactions = await getTransactionsForThisWeek(_id);
-        break;
-      // get all transactions
-      default:
-        transactions = await getAllTransactions(_id);
-    }
-
+    // switch (time) {
+    //   // get transactions for today
+    //   case "today":
+    //     transactions = await getTransactionsForToday(_id);
+    //     break;
+    //   // get transactions for this month
+    //   case "this-month":
+    //     transactions = await getTransactionsForThisMonth(_id);
+    //     break;
+    //   // get transactions for this year
+    //   case "this-week":
+    //     transactions = await getTransactionsForThisWeek(_id);
+    //     break;
+    //   // get all transactions
+    //   default:
+    //     transactions = await getAllTransactions(_id);
+    // }
+    const transactions = [];
     // console.log('transactions:', transactions);
 
     return NextResponse.json(transactions, { status: 200 });
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,49 +2,49 @@ import { NextRequest, NextResponse } from "next/server";
 import { stackServerApp } from "./stack";
 const authPathes = ["/login", "/signup"];
 
-export async function middleware(request: NextRequest) {
-  // console.log("pathname = ", request.nextUrl.pathname);
-  const isAuthPath = authPathes.includes(request.nextUrl.pathname);
+// export async function middleware(request: NextRequest) {
+//   // console.log("pathname = ", request.nextUrl.pathname);
+//   const isAuthPath = authPathes.includes(request.nextUrl.pathname);
 
-  const user = await stackServerApp.getUser();
+//   const user = await stackServerApp.getUser();
 
-  const response = NextResponse.next();
+//   const response = NextResponse.next();
 
-  if (user?.primaryEmail) {
-    // Set custom header
-    response.headers.set("x-user-email", user.primaryEmail);
-  }
+//   if (user?.primaryEmail) {
+//     // Set custom header
+//     response.headers.set("x-user-email", user.primaryEmail);
+//   }
 
-  if (!user) {
-    if (isAuthPath) {
-      return NextResponse.next();
-    }
-    if (request.nextUrl.pathname.startsWith("/api/")) {
-      return new NextResponse("Not authorized", { status: 401 });
-    }
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+//   if (!user) {
+//     if (isAuthPath) {
+//       return NextResponse.next();
+//     }
+//     if (request.nextUrl.pathname.startsWith("/api/")) {
+//       return new NextResponse("Not authorized", { status: 401 });
+//     }
+//     return NextResponse.redirect(new URL("/login", request.url));
+//   }
 
-  if (isAuthPath) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
-  }
+//   if (isAuthPath) {
+//     return NextResponse.redirect(new URL("/dashboard", request.url));
+//   }
 
-  if (
-    !user?.clientMetadata?.onboarded &&
-    request.nextUrl.pathname !== "/onboard"
-  ) {
-    return NextResponse.redirect(new URL("/onboard", request.url));
-  }
+//   if (
+//     !user?.clientMetadata?.onboarded &&
+//     request.nextUrl.pathname !== "/onboard"
+//   ) {
+//     return NextResponse.redirect(new URL("/onboard", request.url));
+//   }
 
-  return response;
-}
+//   return response;
+// }
 
-export const config = {
-  // You can add your own route protection logic here
-  // Make sure not to protect the root URL, as it would prevent users from accessing static Next.js files or Stack's /handler path
-  //   matcher: ["/((?!_next/static|_next/image|favicon.ico|login|signup|$).*)"],
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|$).*)"],
-  // matcher: ["/:path*"],
-};
+// export const config = {
+//   // You can add your own route protection logic here
+//   // Make sure not to protect the root URL, as it would prevent users from accessing static Next.js files or Stack's /handler path
+//   //   matcher: ["/((?!_next/static|_next/image|favicon.ico|login|signup|$).*)"],
+//   matcher: ["/((?!_next/static|_next/image|favicon.ico|$).*)"],
+//   // matcher: ["/:path*"],
+// };
 
-// export default function handler() {}
+export default function handler() {}
