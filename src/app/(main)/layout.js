99d2578051f1a72042cb8e9802dd2eff47e95c8f
Minatallasabri <minatallasabri3380@gmail.com>
2025-01-18 10:26:58 +0200

    LastUpdatesAgain at 18-10

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,9 +1,12 @@
-import SideBarComponent from '@/Components/SideBarComponent'//'../_SideBarComponent';
+import SideBarComponent from '@/Components/SideBarComponent'//'../_SideBarComponent'; 
+import {Flex} from '@chakra-ui/react';
 
 export default function Layout({ children }) {
   return (
     <>
+      <Flex>
       <SideBarComponent>{children}</SideBarComponent>
+      </Flex>
     </>
   );
 }
