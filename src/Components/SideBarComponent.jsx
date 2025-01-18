cccf27638b83c5c0eb9416250df4cc8549190753
Minatallasabri <minatallasabri3380@gmail.com>
2025-01-18 11:08:31 +0200

    I'M-Just-Trying at 18-11

--- src/Components/SideBarComponent.jsx
+++ src/Components/SideBarComponent.jsx
@@ -63,7 +63,7 @@ export default function SideBarComponent({ children }) {
     <div className={styles.container}>
       <aside className={styles.sidebar}>
         <div className={styles.sidebarHeader}>
-          <h1> classname={styles.dancingScriptFont}>Ai-Transact</h1>
+          <h1 classname={styles.dancingScriptFont}>Ai-Transact</h1>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Playwrite+IN:wght@100..400&family=Playwrite+VN:wght@100..400&display=swap" rel="stylesheet" />
--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,12 +1,9 @@
 import SideBarComponent from '@/Components/SideBarComponent'//'../_SideBarComponent'; 
-import {Flex} from '@chakra-ui/react';
 
 export default function Layout({ children }) {
   return (
-    <>
-      <Flex>
+     <>
       <SideBarComponent>{children}</SideBarComponent>
-      </Flex>
     </>
   );
 }
